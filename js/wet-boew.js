683ed631487ecdc0d543895839df06c2ab4e0e8d
Patrick Lajeunesse <patlaj@gmail.com>
2018-08-31 09:25:40 -0400

    trying to get multiple menus

--- js/wet-boew.js
+++ js/wet-boew.js
@@ -6743,12 +6743,12 @@ var componentName = "wb-menu",
 			}
 
 			// Add the site information
-			if ( $info.length !== 0 ) {
-				allProperties.push( [
-					$info.find( "h3, a" ).not( "section a" ),
-					"info-pnl",
-					$info.find( "h2" ).html()
-				] );
+			// if ( $info.length !== 0 ) {
+			// 	allProperties.push( [
+			// 		$info.find( "h3, a" ).not( "section a" ),
+			// 		"info-pnl",
+			// 		$info.find( "h2" ).html()
+			// 	] );
 
 				if ( $info.find( ".wb-navcurr" ).length === 0 ) {
 
